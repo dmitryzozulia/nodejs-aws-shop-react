ddabf9ad9e4e434740ac9b506c2a2b9026859b1c
Dmytro Zozulia <z1dima1z@gmail.com>
2025-02-15 16:19:45 +0100

    feat: add automatic deploy

---
+++ my-web-app-cdk/lib/my-web-app-cdk-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as path from "path";
+import { Construct } from "constructs";
+
+export class MyWebAppCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket to host your static website
+    const websiteBucket = new s3.Bucket(this, "XXXXXXXXXXXXX", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket, {
+          originAccessIdentity: new cloudfront.OriginAccessIdentity(
+            this,
+            "OAI"
+          ),
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      defaultRootObject: "index.html",
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+        },
+      ],
+    });
+
+    // Deploy site contents to S3
+    new s3deploy.BucketDeployment(this, "XXXXXXXXXXXXXXXXX", {
+      sources: [s3deploy.Source.asset(path.join(__dirname, "../../dist"))],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, "DistributionDomainName", {
+      value: distribution.distributionDomainName,
+      description: "Website URL",
+    });
+  }
+}
