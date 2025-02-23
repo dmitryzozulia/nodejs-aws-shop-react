1377837ef1e6b7c8632202236f61c7067ee91373
Dmytro Zozulia <z1dima1z@gmail.com>
2025-02-23 11:27:42 +0100

    feat: change api

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://l3qka918n1.execute-api.us-east-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
@@ -7,3 +7,4 @@ const API_PATHS = {
 };
 
 export default API_PATHS;
+
